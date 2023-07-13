a835a3f82bff6946fb11a2b26bc4cf5439d684aa
Darhlilove <darhlilove16@gmail.com>
2023-07-13 17:26:02 +0000

    Add 4-puts.c script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * message: contains the string to be displayed
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+char message[] = "\"Programming is like building a multilingual puzzle";
+
+puts(message);
+return (0);
+}
