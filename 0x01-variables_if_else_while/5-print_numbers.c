855db399b13b0cb462533c094bbfcab744ebde71
Darhlilove <darhlilove16@gmail.com>
2023-07-14 11:26:36 +0000

    Add 5-print_numbers.c program

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * num - placeholder for numbers
+ *
+ * Return: Always 0 on success
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num); /* Print numbers */
+		num++;
+	}
+
+	return (0);
+}
