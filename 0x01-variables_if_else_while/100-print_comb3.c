986eec783dfe9f770e19cd18fd5f7815fe456a52
Darhlilove <darhlilove16@gmail.com>
2023-07-14 17:47:14 +0000

    Add 100-print_comb3.c program

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * num1 - placeholder for decimal designates for single digit characters 0-8
+ *
+ * num2 - placeholder for decimal designates for single digit characters 1-9
+ *
+ * Return: Always 0 on success
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 48; num1 < 57; num1++)
+	{
+		for (num2 = 49; num2 < 58; num2++)
+		{
+			if (num1 != num2)
+			{
+				putchar(num1);
+				putchar(num2);
+
+				if (num1 == 56 && num2 == 57)
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
