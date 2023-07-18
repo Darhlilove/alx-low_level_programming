ad114a9933c115de37e769e6886bd5f119cd582f
Darhlilove <darhlilove16@gmail.com>
2023-07-18 17:17:50 +0000

    Add 8-24_hours.c program

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 24; num1++)
+	{
+		for (num2 = 0; num < 60; num2++)
+		{
+			_putchar(num1 / 10 + '0');
+			_putchar(num1 % 10 + '0');
+			_putchar(':');
+			_putchar(num2 / 10 + '0');
+			_putchar(num2 % 10 + '0'0);
+		}
+	}
+}
