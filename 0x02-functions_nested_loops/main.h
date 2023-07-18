67a5df993bf67f3ddd8634fb9a99804d6c6eb7a2
Darhlilove <darhlilove16@gmail.com>
2023-07-18 14:27:04 +0000

    Update main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_NAME_H
-#define HEADER_NAME_H
+#ifndef HEADER_H
+#define HEADER_H
 
 #include <unistd.h>
 
@@ -10,9 +10,6 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
 
 #endif
