9ff67b69ec187758b0d53bdea14fb3f6373e045d
Darhlilove <darhlilove16@gmail.com>
2023-07-18 15:12:29 +0000

    Add 2-print_alphabet_x10.c program

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#inlcude "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: void on success
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 1; i < 11; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,24 +7,34 @@ int _putchar(char c);
 /* print letters of the alphabet */
 void print_alphabet(void);
 
+/* prints 10 letters of the alphabet 10 times */
 void print_alphabet_x10(void);
 
+/* checks for lowercase character */
 int _islower(int c);
 
+/* checks for alphabetic character */
 int _isalpha(int c);
 
+/* prints the sign of a number */
 int print_sign(int n);
 
+/* computes the absolute value of an integer */
 int _abs(int);
 
+/* prints the last digit of a number */
 int print_last_digit(int);
 
+/* prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59 */
 void jack_bauer(void);
 
+/* prints the 9 times table, starting with 0 */
 void times_table(void);
 
+/* adds two integers and returns the result */.
 int add(int, int);
 
+/* prints all natural numbers from n to 98, followed by a new line */
 void print_to_98(int n);
 
 #endif
