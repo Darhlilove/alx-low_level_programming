a4b53b0a61c7ea881817db3b26ef4825a08d1100
Darhlilove <darhlilove16@gmail.com>
2023-07-18 16:45:37 +0000

    Add 7-print_last_digit.c program

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * @num: placeholder for number to be operated on
+ *
+ * Return: result
+ */
+
+int print_last_digit(int num)
+{
+	result = num % 10;
+	return (result);
+}
