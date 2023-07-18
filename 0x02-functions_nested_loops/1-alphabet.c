a3368895b02283fb802557d0d46e6b1f44ea694b
Darhlilove <darhlilove16@gmail.com>
2023-07-18 14:46:07 +0000

    Add 1-alphabet.c and main.h file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 always on success
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
