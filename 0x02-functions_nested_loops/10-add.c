fad4d744f8ad6ac9be876d221d586dce8439c895
Darhlilove <darhlilove16@gmail.com>
2023-07-18 20:01:38 +0000

    Add 10-add.c program

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * @num1: placeholder for first number
+ *
+ * @num2: placeholder for second number
+ *
+ * Return: result always
+ */
+
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}
