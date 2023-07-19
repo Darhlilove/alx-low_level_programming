3e3da21d2a2c3e7b9cd7f7e71d4c1232443e8fc6
Darhlilove <darhlilove16@gmail.com>
2023-07-19 16:30:37 +0000

    Add 101-natural.c program

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * sum_multiple_of_three_or_five(int n) - Entry point
+ *
+ * Return: 0 always
+ */
+
+int sum_multiple_of_three_or_five(void);
+{
+	int num, result;
+
+	result = 0;
+	for (num == 1; num < 1024; num++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			result = result + num;
+		}
+	}
+	printf('%d', result);
+}
