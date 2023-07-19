bdef2ff05562caa56114c4a0f3107c86d51bd400
Darhlilove <darhlilove16@gmail.com>
2023-07-19 15:55:16 +0000

    Add 100-times_table.c program

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table starting from 0.
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	while (n >= 0 && n <= 15)
+	{
+		int i, j, product;
+
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				product = i * j;
+				if (product <= 9)
+				{
+					if (j > 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_puthcar(' ');
+					}
+					_putchar(product + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
