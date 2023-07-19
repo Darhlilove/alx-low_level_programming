fd6b7e219a1d29ec71fd4b57b55d3391fca28801
Darhlilove <darhlilove16@gmail.com>
2023-07-19 16:34:04 +0000

    Update program

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,9 +12,9 @@ int sum_multiple_of_three_or_five(void)
 	int num, result;
 
 	result = 0;
-	for (num == 1; num < 1024; num++)
+	for (num = 1; num < 1024; num++)
 	{
-		if (n % 3 == 0 || n % 5 == 0)
+		if (num % 3 == 0 || num % 5 == 0)
 		{
 			result = result + num;
 		}
