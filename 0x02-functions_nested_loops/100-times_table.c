dc1ae3636e004e880c323e9ccde95c4bd788cad4
Darhlilove <darhlilove16@gmail.com>
2023-07-19 15:57:02 +0000

    Update program

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,7 +5,7 @@
  *
  * Return: void
  */
-void times_table(void)
+void print_times_table(int n)
 {
 	while (n >= 0 && n <= 15)
 	{
@@ -23,7 +23,7 @@ void times_table(void)
 						_putchar(',');
 						_putchar(' ');
 						_putchar(' ');
-						_puthcar(' ');
+						_putchar(' ');
 					}
 					_putchar(product + '0');
 				}
