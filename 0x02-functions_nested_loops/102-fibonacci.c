e25bc1313b5391d56ddce02d1711cd4e1368e4e3
Darhlilove <darhlilove16@gmail.com>
2023-07-20 09:02:42 +0000

    Update program

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -32,13 +32,13 @@ int main(void)
 
 void fibonacci(int n)
 {
-	int num1, num2, result, count, cond;
+	long int num1, num2, result, count, cond;
 
 	num1 = 1;
 	num2 = 2;
 
-	printf("%d, ", num1);
-	printf("%d, ", num2);
+	printf("%ld, ", num1);
+	printf("%ld, ", num2);
 
 	cond = n - 2; /* counter for n-2 observations */
 	for (count = 1; count <= cond; count++)
@@ -47,11 +47,11 @@ void fibonacci(int n)
 
 		if (count != cond)
 		{
-			printf("%lld, ", result);
+			printf("%ld, ", result);
 		}
 		else
 		{
-			printf("%lld\n", result);
+			printf("%ld\n", result);
 		}
 
 		num1 = num2;
