174bf0f56cfee7a46e07569fc8a46c74253eb015
Darhlilove <darhlilove16@gmail.com>
2023-07-21 09:04:21 +0000

    Add 3-print_numbers.c program

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers 0 to 9
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void);
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+}
