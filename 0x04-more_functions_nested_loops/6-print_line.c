007c8935dad8e0113ba0a5ac5e2977fddc860bea
Darhlilove <darhlilove16@gmail.com>
2023-07-21 10:08:08 +0000

    Update program

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,9 +12,12 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 1; i <= n; i++)
 	{
-		_putchar('_');
+		if (!(i < 0))
+		{
+			putchar('_');
+		}
 	}
-	_putchar('\n');
+	putchar('\n');
 }
