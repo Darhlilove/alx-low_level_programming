c462c1961bbaef4b9bbbb62de221c4252b35c235
Darhlilove <darhlilove16@gmail.com>
2023-07-28 11:36:20 +0000

    Update program

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,13 +10,10 @@
 
 void _puts(char *str)
 {
-	char array[] = *str;
-	int i = 0;
-
-	while (array[i] != '\0')
+	while (*str != '\0')
 	{
-		_putchar(array[i]);
-		i++;
+		_putchar(*str);
+		str++;
 	}
 
 	_putchar('\n');
