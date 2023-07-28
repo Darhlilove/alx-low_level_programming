b5bbe671b2e333489e859e7a74b865eefe859cbb
Darhlilove <darhlilove16@gmail.com>
2023-07-28 10:14:01 +0000

    Add 1-swap.c program

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers
+ *
+ * @a: placeholder for first integer
+ * @b: placeholder for second integer
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+
+	return (void);
+}
