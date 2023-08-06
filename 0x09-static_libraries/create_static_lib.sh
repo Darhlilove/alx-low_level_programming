6e31e037fef7ada1aa2e1817fd6868bdfb828535
Darhlilove <darhlilove16@gmail.com>
2023-08-06 12:43:58 +0000

    Update script

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 # Compile all .c files in the current directory into object files
-gcc -Wall -Werror -Wextra -pedantic -c *.c
+gcc -c *.c
 
 # Create a static library named liball.a from the compiled object files
 ar -rc liball.a *.o
-
-# Index the static library
-ranlib liball.a
-
-# Remove the compiled object files
-rm *.o
