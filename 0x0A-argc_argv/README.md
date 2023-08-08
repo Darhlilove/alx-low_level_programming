f508fdf55df5f1fe6cec582589f3e7000bd76ece
Darhlilove <darhlilove16@gmail.com>
2023-08-08 12:48:05 +0000

    Add program files

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is a project on arguments in the \"main\" function in C.
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_ARGS
+#define HEADER_ARGS
+
+/* prints character to stdout */
+int _putchar(char c);
+
+#endif /* HEADER_ARGS */
