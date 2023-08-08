76b8d9c048c5c69ef453feff19d1fcec8ec3405c
Darhlilove <darhlilove16@gmail.com>
2023-08-08 15:12:22 +0000

    Update header file

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,9 +1,15 @@
 #ifndef HEADER_ARGS
 #define HEADER_ARGS
 
+#include <stdio.h>
+
+/* Convert string to int */
+int atoi(char *num);
+
 /* prints character to stdout */
 int _putchar(char c);
 
+/* main function */
 int main(int argc, char *argv[]);
 
 #endif /* HEADER_ARGS */
