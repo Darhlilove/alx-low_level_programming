8c4b3a5241fc9c74a403f3ea93b4fe1c77c03f9b
Darhlilove <darhlilove16@gmail.com>
2023-08-09 12:55:59 +0000

    Add program files

---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+This is a project on memory allocation and deallocation using malloc and free,
+respectively.
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_ALLOC
+#define HEADER_ALLOC
+
+#include <stdlib.h>
+
+int _putchar(char c);
+
+char *create_array(unsigned int size, char c);
+
+char *_strdup(char *str);
+
+char *str_concat(char *s1, char *s2);
+
+int **alloc_grid(int width, int height);
+
+void free_grid(int **grid, int height);
+
+#endif /* HEADER_ALLOC */
