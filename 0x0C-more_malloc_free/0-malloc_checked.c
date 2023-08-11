ef59007f65e25a12feaf32d6d61091bd54a1644d
Darhlilove <darhlilove16@gmail.com>
2023-08-11 11:00:38 +0000

    Update program files

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,6 @@
 
 /**
  * malloc_checked - Allocates memory using malloc.
- *
  * @b: The number of bytes to allocate.
  *
  * Return: A pointer to the allocated memory.
@@ -10,9 +9,7 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
-
-	ptr = malloc(b);
+	void *ptr = malloc(b);
 
 	if (ptr == NULL)
 		exit(98);
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,17 @@
 #ifndef HEADER_MALLOC
 #define HEADER_MALLOC
 
-#include <stlib.h>
+#include <stdlib.h>
 
 int _putchar(char c);
+
+void *malloc_checked(unsigned int b);
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+int *array_range(int min, int max);
+
 
 #endif /* HEADER_MALLOC */
