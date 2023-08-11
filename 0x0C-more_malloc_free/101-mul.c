64a1d7afac48a7630903fac573da73ae75619875
Darhlilove <darhlilove16@gmail.com>
2023-08-11 13:04:56 +0000

    Update program files

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,22 +1,107 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
- * check_digit - Check if a string is a valid positive number
+ * _strlen - Calculate the length of a string
  *
- * @s: The string to check
+ * @s: The input string
  *
- * Return: 1 if valid, 0 if not
+ * Return: The length of the string
  */
-
-int check_digit(char *s)
+int _strlen(char *s)
 {
+	int len = 0;
+
 	while (*s)
 	{
-		if (*s < '0' || *s > '9')
-			return (0);
+		len++;
 		s++;
 	}
-	return (1);
+	return (len);
+}
+
+/**
+ * _strdup - Duplicate a string using malloc
+ *
+ * @str: The input string
+ *
+ * Return: A pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	int len = _strlen(str) + 1;
+	char *dup = malloc(len);
+	int i;
+
+	if (dup == NULL)
+	{
+		exit(98);
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dup[i] = str[i];
+	}
+
+	return (dup);
+}
+
+/**
+ * _atoi - Convert a string to an integer
+ *
+ * @s: The string to convert
+ *
+ * Return: The converted integer
+ */
+int _atoi(char *s)
+{
+	int res = 0;
+
+	while (*s)
+	{
+		res = res * 10 + (*s - '0');
+		s++;
+	}
+	return (res);
+}
+
+/**
+ * multiply - Multiply two numbers stored as strings
+ *
+ * @num1: The first number
+ * @num2: The second number
+ *
+ * Return: A pointer to the result
+ */
+int *multiply(char *num1, char *num2)
+{
+	int len1 = _strlen(num1);
+	int len2 = _strlen(num2);
+	int len_result = len1 + len2;
+	int *result = malloc(sizeof(int) * len_result);
+	int i, j;
+
+	if (result == NULL)
+		exit(98);
+
+	for (i = 0; i < len_result; i++)
+		result[i] = 0;
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		int carry = 0;
+
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			int product = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1] + carry;
+
+			carry = product / 10;
+			result[i + j + 1] = product % 10;
+		}
+		result[i] += carry;
+	}
+
+	return (result);
 }
 
 /**
@@ -27,41 +112,35 @@ int check_digit(char *s)
  *
  * Return: 0 on success, 98 on error
  */
-
 int main(int argc, char *argv[])
 {
-	int len1, len2, len_sum, i, j;
+	char *num1, *num2;
+	int i, start;
 	int *result;
 
-	if (argc != 3 || !check_digit(argv[1]) || !check_digit(argv[2]))
+	if (argc != 3)
 	{
-		printf("Error\n");
-		return (98);
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+		exit(98);
 	}
 
-	len1 = strlen(argv[1]);
-	len2 = strlen(argv[2]);
-	len_sum = len1 + len2;
+	num1 = _strdup(argv[1]);
+	num2 = _strdup(argv[2]);
+	result = multiply(num1, num2);
+	start = (result[0] == 0) ? 1 : 0;
 
-	result = calloc(len_sum, sizeof(int));
-	if (result == NULL)
-		return (98);
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			result[i + j + 1] += (argv[1][i] - '0')
-				* (argv[2][j] - '0');
-			result[i + j] += result[i + j + 1] / 10;
-			result[i + j + 1] %= 10;
-		}
-	}
-
-	for (i = 0; i < len_sum; i++)
+	for (i = start; i < _strlen(num1) + _strlen(num2); i++)
 		_putchar(result[i] + '0');
 	_putchar('\n');
 
+	free(num1);
+	free(num2);
 	free(result);
+
 	return (0);
 }
