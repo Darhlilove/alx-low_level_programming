56b2c4326d3481f2a90747af7e8f6e0225b53c92
Darhlilove Botchway <101454759+Darhlilove@users.noreply.github.com>
2023-09-05 08:23:30 +0000

    Delete 0x0A-argc_argv directory

--- 0x0A-argc_argv/README.md
+++
@@ -1,1 +0,0 @@
-This is a project on arguments in the \"main\" function in C.
--- 0x0A-argc_argv/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEADER_ARGS
-#define HEADER_ARGS
-
-#include <stdio.h>
-#include <ctype.h>
-
-int _putchar(char c);
-
-#endif /* HEADER_ARGS */
